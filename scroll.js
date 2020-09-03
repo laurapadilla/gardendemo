4abcb86d29fa716dc0fdf02eb3f5a9b5ab9fc525
Laura Padilla <lauvpadilla@gmail.com>
2020-09-03 13:29:23 -0400

    update html, css, js

--- scroll.js
+++ scroll.js
@@ -1,5 +1,5 @@
 $(document).on("scroll", function () {
   var pixels = $(document).scrollTop();
 
-  $("section.main").css("left", -1 * pixels);
+  $("section.container").css("left", -1 * pixels);
 });
